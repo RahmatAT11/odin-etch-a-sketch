4cc2f1bed84cbc71327653db29bcd91640a6d574
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-08-02 09:00:49 +0700

    Add variables to how many times loop

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const sketchContainer = document.querySelector(".container");
+let height = 16;
+let width = 16;
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < height * width; i++) {
     const boxGrid = document.createElement("div");
     boxGrid.classList.add("box");
     
