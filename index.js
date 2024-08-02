d7f2f4b52508e67548be9d72647d875a0bb0cb49
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-08-02 09:35:46 +0700

    Add feature to change box color to red

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const sketchContainer = document.querySelector(".container");
+
 let height = 16;
 let width = 16;
+
+sketchContainer.style.height = "480px";
+sketchContainer.style.width = "480px";
 
 for (let i = 0; i < height * width; i++) {
     const boxGrid = document.createElement("div");
     boxGrid.classList.add("box");
+    boxGrid.addEventListener("mouseover", (e) => {
+        e.target.style.backgroundColor = "red";
+    })
     
     sketchContainer.appendChild(boxGrid);
 }
