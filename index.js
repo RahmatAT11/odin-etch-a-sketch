169dbe7a551500bee9fa696aaaaedb3e286eb6d0
Rahmat Alrafi Trikada <ralrafi12@gmail.com>
2024-08-02 10:18:45 +0700

    Add feature to allow setting grid size

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
-const sketchContainer = document.querySelector(".container");
+let sketchContainer = document.querySelector(".container");
+const btnSetGrid = document.querySelector("#set-grid");
 
-let height = 16;
-let width = 16;
+let height, width;
+height = 16;
+width = 16;
 
 sketchContainer.style.height = "480px";
 sketchContainer.style.width = "480px";
 
-for (let i = 0; i < height * width; i++) {
-    const boxGrid = document.createElement("div");
-    boxGrid.classList.add("box");
-    boxGrid.addEventListener("mouseover", (e) => {
-        e.target.style.backgroundColor = "red";
-    })
+btnSetGrid.addEventListener("click", (e) => {
+    let sizeInput;
     
-    sketchContainer.appendChild(boxGrid);
+    do {
+        sizeInput = +prompt("Please enter number from 1-100:");
+        height = sizeInput;
+        width = sizeInput;
+    } while (!(sizeInput < 101 && sizeInput > 0));
+
+    document.querySelector("body").removeChild(sketchContainer);
+    sketchContainer = document.createElement("div");
+    sketchContainer.classList.add("container");
+
+    sketchContainer.style.height = height * 30 + "px";
+    sketchContainer.style.width = width * 30 + "px";
+
+    setGrid(height, width);
+
+    document.querySelector("body").appendChild(sketchContainer);
+})
+
+function setGrid(height, width) {
+    for (let i = 0; i < height * width; i++) {
+        const boxGrid = document.createElement("div");
+        boxGrid.classList.add("box");
+        boxGrid.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "red";
+        })
+        
+        sketchContainer.appendChild(boxGrid);
+    }
 }
+
+setGrid(height, width);
